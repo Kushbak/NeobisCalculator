0bcc73dae8687a2b9a5bb0758b12cd18e6e88fd6
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-09 15:46:14 +0600

    added all ready files

---
+++ js/script.js
@@ -0,0 +1,55 @@
+let nums = document.querySelectorAll('.num'); 
+let equals = document.querySelector('.equals');
+let output = document.querySelector('.output');
+let allClear = document.querySelector('.allClear');
+let clear = document.querySelector('.clear');
+let operators = document.querySelectorAll('.operator');
+
+
+
+// for output
+let outputFunc = (num) => {
+	output.value += num
+}
+
+
+
+// for clear whole output
+let allClearFunc = () => {
+	output.value = ''
+}
+allClear.addEventListener('click', () => allClearFunc())
+ 
+
+
+// for clear last of output
+let clearFunc = () => {
+	output.value = output.value.substring(0, output.value.length - 1) 
+}
+clear.addEventListener('click', () => clearFunc())
+
+
+
+
+// for insert equals
+let equalsFunc = () => {
+	if(output.value){
+		output.value = eval(output.value)
+	}
+}
+equals.addEventListener('click', () => equalsFunc())
+
+
+
+// every tap on btn should call func and output  their values
+nums.forEach(num => num.addEventListener('click', () => outputFunc(num.innerHTML)))
+
+
+operators.forEach(op => {
+	op.addEventListener('click', () => {
+		if(output.value[output.value.length - 1] != op){
+			console.log(output.value[output.value.length - 1]);
+			outputFunc(op.innerHTML) 
+		}
+	})
+})
